acee8bcccd483c08dd160b976401e23149d6ed33
Sara-Castaneda25 <saraletsbefluent@gmail.com>
2023-11-04 21:41:50 -0500

    Primer commit

---
+++ CS4/ejercicio.js
@@ -0,0 +1,33 @@
+
+const prompt = requiere("prompt-sync")({signit: true});
+let nombre = prompt("Ingrese su nombre");
+console.log(`Hola ${nombre}!`);
+
+//Situación 1: Una persona quiere registrar los datos de su mascota
+let nom = "Tobby";
+let edad_mascota = 3;
+let estaVacunado = true;
+let raza = "criollo";
+let sufreEnfermedades = false;
+let peso = 13.5;
+
+
+//Situación 2: Datos de una persona en un aeropuerto
+
+let nom2 = "Sara";
+let apellidos = "Castañeda Echeverri";
+let documento = "1033177099";
+let edad = 19;
+let esMayor = true;
+let tipoViaje = "internacional";
+let tieneAcompañantes = false;
+let checkIn = true;
+let diasPorFuera = 10;
+
+
+
+
+
+
+
+
